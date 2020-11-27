e1463394028d5bf057c6b57da89ef7c6ecda856f
Justus K <justus.k@protonmail.com>
2020-11-27 10:47:23 +0100

    feat: add `Quantity` and `Unit`

--- README.md
+++ README.md
@@ -4,6 +4,25 @@ smol and fast version of [`uom`]
 
 [Documentation][docs-rs] | [Crate][crates-io] | [Examples][examples]
 
+This crate is mostly based on [this] proof-of-concept using const generics.
+`tiny-uom` is a port of `uom` to use const generics and to be a faster and smaller version.
+It provides type-safe and zero-cost [dimensional-analysis].
+`tiny-uom` provides all units that are specified in the [International System of Units][SI].
+
+## Usage
+
+`tiny-uom` requires nightly Rust compiler (currently it's always tested with `nightly-2020-11-25`).
+Add this your `Cargo.toml`:
+
+```toml
+[dependencies]
+tiny-uom = "0.1.0"
+```
+
+## Design
+
+coming soon...
+
 ### License
 
 This project is licensed under the [MIT][license] license
@@ -13,3 +32,6 @@ This project is licensed under the [MIT][license] license
 [crates-io]: https://crates.io/crates/tiny-uom
 [examples]: https://github.com/Stupremee/tiny-uom/tree/main/examples
 [license]: https://github.com/Stupremee/tiny-uom/tree/main/LICENSE
+[this]: https://docs.rs/const_unit_poc
+[dimensional-analysis]: https://en.wikipedia.org/wiki/Dimensional_analysis
+[SI]: https://jcgm.bipm.org/vim/en/1.16.html
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,57 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+//! todo
+#![deny(
+    rust_2018_idioms,
+    warnings,
+    clippy::pedantic,
+    missing_docs,
+    missing_debug_implementations,
+    broken_intra_doc_links,
+    unsafe_code
+)]
+#![allow(incomplete_features)]
+#![feature(const_generics)]
+
+/// The `Unit` struct can represent every possible unit
+/// that is defined in the [`SI`] system.
+///
+/// It is able to do so because it contains a list of all
+/// 7 base units and a number which represents the exponent
+/// of that unit.
+///
+/// # Example
+///
+/// ## Newton
+/// ```no_rust
+/// kg * m * s⁻²
+/// ```
+///
+/// would be represented using the following `Unit`:
+/// ```no_rust
+/// Unit {
+///     m: 1,
+///     kg: 1,
+///     s: -2,
+/// }
+/// ```
+///
+/// [`SI`]: https://jcgm.bipm.org/vim/en/1.16.html
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[allow(non_snake_case)]
+pub struct Unit {
+    m: i8,
+    kg: i8,
+    s: i8,
+    A: i8,
+    K: i8,
+    mol: i8,
+    cd: i8,
+}
+
+/// A `Quantity` represents a raw value and it's unit
+/// that is represented as a const generic parameter.
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[repr(transparent)]
+pub struct Quantity<const U: Unit> {
+    /// The raw value of this `Quantity`
+    pub value: f64,
 }
